3c6d08a43ca69e6fc1a32a0538b93adb1cb0a285
VictorSichun <victorsichun@gmail.com>
2018-04-22 21:26:13 +1200

    Add files via upload
    Currently, WebCrawler works for retrieving images on a web page, but somehow cannot retrieve all of them

--- WebCrawler.py
+++ WebCrawler.py
@@ -1,16 +1,41 @@
+import os
 import urllib.request
+from urllib.parse import urljoin
+from bs4 import BeautifulSoup
+import ssl
 
-unvisited_url = ['http://www.baidu.com/']
+unvisited_url = ['https://www.zhihu.com/question/38473122']
 visited_url = []
 working_url = ''
-file_name = 'crawler_testing_file'
-parent_path = '/Users/victor/Documents/'
+folder_name = 'crawler_testing_file'
+parent_path = '/Users/victor/Desktop/'
+current_directory = os.path.join(parent_path, folder_name)
+
+
+# bypass verification
+#context = ssl._create_unverified_context()
 while len(unvisited_url) > 0:
     working_url = unvisited_url.pop(0)
     visited_url.append(working_url)
-    contents = urllib.request.urlopen(working_url).read()
-    working_file = open(parent_path + file_name, 'wb')
-    working_file.write(contents)
+    opening_page = urllib.request.urlopen(working_url)
+    page_content = BeautifulSoup(opening_page, "html.parser")
+    fetched_content = page_content.findAll('img')
+
+    for i in fetched_content:
+        try:
+            img_url = urljoin(working_url, i['data-actualsrc'])
+            file_name = i['src'].split('/')[-1]
+            # check if the directory exists; if not, create a new one
+            if not os.path.exists(current_directory):
+                os.makedirs(current_directory)
+
+            file_path = os.path.join(current_directory, file_name)
+            urllib.request.urlretrieve(img_url, file_path)
+        except Exception as e:
+            print(e)
+            continue
+
+    urllib.request.urlcleanup()
     '''
     if content is HTML
         parse out URLs from links
@@ -19,7 +44,4 @@ while len(unvisited_url) > 0:
               and it's not already in either the visited or unvisited list
               add it to the unvisited list
     '''
-working_file.close()
 print("success!")
-
-
